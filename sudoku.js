fdd95838b2de212e52066a00d3ee42d328a8d227
readonlychild <neto.lopez@gmail.com>
2015-05-06 00:26:45 -0500

    version 1.0

---
+++ sudoku.js
@@ -0,0 +1,270 @@
+var sudoku = sudoku || {};
+
+sudoku.container = "#sudoku";
+sudoku.messageContainer = "#sudo-message";
+sudoku.currX = 0;
+sudoku.currY = 0;
+sudoku.hintsCount = 28;
+sudoku.unsolvableFlag = true;
+
+sudoku.initialize = function () {
+	
+	var instance = this;
+
+	// CELL CLICKING
+	$(this.container).on("click", ".cell", function () {
+		var $this = $(this);
+		var x = parseInt($this.data("col"));
+		var y = parseInt($this.data("row"));
+		instance.currX = x;
+		instance.currY = y;
+		$(instance.container + " .cell").removeClass("active")
+		$this.addClass("active");
+		instance.render();
+	});
+
+	// DOC KEYPRESS
+	$(document).on("keyup", function (evt) {
+		
+		//up 38
+		if (evt.keyCode == 38) {
+			if (instance.currY == 0) return;
+			instance.currY --;
+			instance.render();
+		}
+		//dn 40
+		if (evt.keyCode == 40) {
+			if (instance.currY == 8) return;
+			instance.currY ++;
+			instance.render();
+		}
+		//left 37
+		if (evt.keyCode == 37) {
+			if (instance.currX == 0) return;
+			instance.currX --;
+			instance.render();
+		}
+		//right 39
+		if (evt.keyCode == 39) {
+			if (instance.currX == 8) return;
+			instance.currX ++;
+			instance.render();
+		}
+
+		//console.log(evt);
+		if (instance.isCellLocked(instance.currY, instance.currX)) {
+			//console.log('cell is locked');
+			return;
+		}
+
+		//console.log(evt.keyCode);
+		if (evt.keyCode >= 49 && evt.keyCode <= 57) {
+			instance.setValue(evt.keyCode - 48);
+			return;
+		}
+		// cero blank cell - spacebar too
+		if (evt.keyCode == 48 || evt.keyCode == 32) {
+			instance.setValue(0);
+		}
+		
+	});
+	
+	// BTN-DIGIT ACTION
+	$(document).on("click", ".btn-digit", function () {
+		var d = parseInt($(this).data("digit"));
+		instance.setValue(d);
+	});
+
+};
+
+sudoku.render = function () {
+	this._getBlockValidity();
+
+	var allfilled = true;
+	var allvalids = true;
+
+	var htm = "<table class='sudo-grid'>";
+	for (var i = 0; i < this.cells.length; i++) {
+		var row = this.cells[i];
+		htm += "<tr>";
+		for (var j = 0; j < row.length; j++) {
+
+			if (row[j] == 0) allfilled = false;
+
+			htm += "<td class='cell";
+			var vcss = "";
+			if (this.currX == j) vcss = " crosshair";
+			if (this.currY == i) vcss = " crosshair";
+			if (this.currX == j && this.currY == i) vcss = " active";
+			if (this._altcell(i, j)) { vcss += " alt"; }
+
+			//vcss += " " + this._getblockcode(i, j);
+
+			if (this._blockValidity[i][j] == 0) vcss += " block-ng";
+
+			if (this.isCellLocked(i, j)) vcss += " locked";
+
+			htm += vcss;
+			htm += "' data-row='" + i + "' data-col='" + j + "'>" + (row[j] == 0 ? "&nbsp;" : row[j]) + "</td>";
+		}
+		var rowisvalid = this.isRowValid(i);
+		if (!rowisvalid) allvalids = false;
+		htm += "<td class='" + (rowisvalid ? "ok" : "ng") + "''>&nbsp;</td>";
+		htm += "</tr>";
+	}
+	htm += "<tr>";
+	for (var i = 0; i < this.cells.length; i++) {
+		var colisvalid = this.isColValid(i);
+		if (!colisvalid) allvalids = false;
+		htm += "<td class='" + (colisvalid ? "ok" : "ng") + "''>&nbsp;</td>";
+	}
+	if (this.unsolvableFlag) {
+		var stillsolvable = this.solver.isSolvable(this.cells);
+		htm += "<td class='" + (stillsolvable ? "ok" : "ng") + "'>&nbsp;</td>";
+	}
+	htm += "</tr>";
+	htm += "</table>";
+	$(this.container).html(htm);
+	if (allvalids && allfilled) {
+		this.say("you are a winner!", "showing");
+	} else {
+		this.say("");
+	}
+};
+sudoku._altcell = function (i, j) {
+	if (i < 3 || i > 5) {
+		if (j < 3) return true;
+		if (j > 5) return true;
+	} else {
+		if (j > 2 && j < 6) return true;
+	}
+	return false;
+}
+sudoku.setValue = function (val, x, y) {
+	x = x || this.currX;
+	y = y || this.currY;
+	this.cells[y][x] = val;
+	this.render();
+};
+
+sudoku.isCellLocked = function (i, j) {
+	return this.gridlock[i][j] > 0;
+};
+sudoku.isRowValid = function (idx) {
+	//check total limit
+	var s = 0;
+	for (var i = 0; i < this.cells.length; i++) {
+		var val = this.cells[idx][i];
+		s += val;
+	}
+	if (s > 45) return false;
+	// check no repeated digits
+	var dig = [];
+	for (var i = 0; i < this.cells.length; i++) {
+		dig[i] = 0;
+	}
+	for (var i = 0; i < this.cells.length; i++) {
+		var val = this.cells[idx][i] - 1;
+		dig[val] += 1;
+	}
+	for (var i = 0; i < this.cells.length; i++) {
+		if (dig[i] > 1) return false;
+	}
+	return true;
+};
+sudoku.isColValid = function (idx) {
+	//check total limit
+	var s = 0;
+	for (var i = 0; i < this.cells.length; i++) {
+		var val = this.cells[i][idx];
+		s += val;
+	}
+	if (s > 45) return false;
+	// check no repeated digits
+	var dig = [];
+	for (var i = 0; i < this.cells.length; i++) {
+		dig[i] = 0;
+	}
+	for (var i = 0; i < this.cells.length; i++) {
+		var val = this.cells[i][idx] - 1;
+		dig[val] += 1;
+	}
+	for (var i = 0; i < this.cells.length; i++) {
+		if (dig[i] > 1) return false;
+	}
+	return true;	
+};
+sudoku.isBlockValid = function (x, y, isCellCoords) {
+	if (isCellCoords) {
+		x = parseInt(x/3);
+		y = parseInt(y/3);
+	}
+	// check repeated digits
+	var dig = [];
+	var s = 0;
+	for (var i = 0; i < 9; i++) {
+		dig[i] = 0;
+	}
+	for (var i = 0; i < 3; i++) {
+		for (var j = 0; j < 3; j++) {
+			var val = this.cells[y*3+j][x*3+i] - 1;
+			dig[val] += 1;
+			s += val + 1;
+			//console.log('dig',y*3+j, x*3+i, val + 1);
+		}
+	}
+	for (var i = 0; i < 9; i++) {
+		if (dig[i] > 1) return false;
+	}
+	if (s > 45) return false;
+	return true;
+};
+sudoku._getBlockValidity = function () {
+	var rowsx = [];
+	for (var i = 0; i < 9; i++) {
+		rowsx.push([1,1,1,1,1,1,1,1,1]);
+		for (var j = 0; j < 9; j++) {
+			var x = parseInt(j/3);
+			var y = parseInt(i/3);
+			//rowsx[i,j] = this.isBlockValid(i, j, true) ? 1 : 0;
+			rowsx[i][j] = this.isBlockValid(x, y) ? 1 : 0;
+			//console.log(x + "," + y, i + "," + j, rowsx[i][j]);
+		}
+	}
+	this._blockValidity = rowsx;
+	//return rows;
+};
+
+sudoku.startGame = function (hintCount) {
+	hintCount = hintCount || this.hintsCount;
+
+	var grid = this.solver.generatePuzzle(hintCount);
+
+	// build gridlock
+	var gridl = [];
+	gridl.push([0,0,0,0,0,0,0,0,0]);
+	gridl.push([0,0,0,0,0,0,0,0,0]);
+	gridl.push([0,0,0,0,0,0,0,0,0]);
+	gridl.push([0,0,0,0,0,0,0,0,0]);
+	gridl.push([0,0,0,0,0,0,0,0,0]);
+	gridl.push([0,0,0,0,0,0,0,0,0]);
+	gridl.push([0,0,0,0,0,0,0,0,0]);
+	gridl.push([0,0,0,0,0,0,0,0,0]);
+	gridl.push([0,0,0,0,0,0,0,0,0]);
+
+	for (var i = 0; i < gridl.length; i++) {
+		for (var j = 0; j < gridl.length; j++) {
+			if (grid[i][j] > 0) gridl[i][j] = 1;
+		}
+	}
+	this.gridlock = gridl;
+	this.cells = grid;
+	this._getBlockValidity();
+	return grid;
+};
+
+sudoku.say = function (msg, cssClass) {
+	cssClass = cssClass || "";
+	$(this.messageContainer).removeClass("showing");
+	$(this.messageContainer).html(msg).addClass(cssClass);
+};
---
+++ sudoku.solver.js
@@ -0,0 +1,234 @@
+/**
+snippets/functions out of 
+https://www.codefellows.org/blog/sudoku-solver-from-scratch-in-javascript-tdd-style-a-tutorial
+**/
+
+var sudoku = sudoku || {};
+sudoku.solver = {};
+sudoku.solver.saveEmptyPositions = function(board) {
+  // Create an array to save the positions
+  var emptyPositions = [];
+
+  // Check every square in the puzzle for a zero
+  for(var i = 0; i < board.length; i++) {
+    for(var j = 0; j < board[i].length; j++) {
+      // If a zero is found, save that position
+      if(board[i][j] === 0) {
+        emptyPositions.push([i, j]);
+      }
+    }
+  }
+
+  // Return the positions
+  return emptyPositions;
+};
+sudoku.solver.checkRow = function(board, row, value) {
+  // Iterate through every value in the row
+  for(var i = 0; i < board[row].length; i++) {
+    // If a match is found, return false
+    if(board[row][i] === value) {
+      return false;
+    }
+  }
+  // If no match was found, return true
+  return true;
+};
+sudoku.solver.checkColumn = function(board, column, value) {
+  // Iterate through each value in the column
+  for(var i = 0; i < board.length; i++) {
+    // If a match is found, return false
+    if(board[i][column] === value) {
+      return false;
+    }
+  }
+  // If no match was found, return true
+  return true;
+};
+sudoku.solver.check3x3Square = function(board, column, row, value) {
+  // Save the upper left corner
+  var columnCorner = 0,
+      rowCorner = 0,
+      squareSize = 3;
+
+  // Find the left-most column
+  while(column >= columnCorner + squareSize) {
+    columnCorner += squareSize;
+  }
+
+  // Find the upper-most row
+  while(row >= rowCorner + squareSize) {
+    rowCorner += squareSize;
+  }
+
+  // Iterate through each row
+  for(var i = rowCorner; i < rowCorner + squareSize; i++) {
+    // Iterate through each column
+    for(var j = columnCorner; j < columnCorner + squareSize; j++) {
+      // Return false is a match is found
+      if(board[i][j] === value) {        
+        return false;
+      }
+    }
+  }
+  // If no match was found, return true
+  return true;
+};
+sudoku.solver.checkValue = function(board, column, row, value) {
+  if(this.checkRow(board, row, value) &&
+    this.checkColumn(board, column, value) &&
+    this.check3x3Square(board, column, row, value)) {
+    return true;
+  } else {
+    return false;
+  }
+};
+sudoku.solver.solvePuzzle = function(board, emptyPositions) {
+  
+  if (!emptyPositions) {
+    emptyPositions = this.saveEmptyPositions(board);
+  }
+  
+  // Variables to track our position in the solver
+  var limit = 9,
+      i, row, column, value, found;
+  for(i = 0; i < emptyPositions.length;) {
+    row = emptyPositions[i][0];
+    column = emptyPositions[i][1];
+    // Try the next value
+    value = board[row][column] + 1;
+    // Was a valid number found?
+    found = false;
+    // Keep trying new values until either the limit
+    // was reached or a valid value was found
+    while(!found && value <= limit) {
+      // If a valid value is found, mark found true,
+      // set the position to the value, and move to the
+      // next position
+      if(this.checkValue(board, column, row, value)) {
+        found = true;
+        board[row][column] = value;
+        i++;
+      } 
+      // Otherwise, try the next value
+      else {
+        value++;
+      }
+    }
+    // If no valid value was found and the limit was
+    // reached, move back to the previous position
+    if(!found) {
+      board[row][column] = 0;
+      i--;
+    }
+  }
+
+  // A solution was found! Log it
+  board.forEach(function(row) {
+    //console.log(row.join());
+  });
+
+  // return the solution
+  return board;
+};
+sudoku.solver.putHint = function (board) {
+  var clone = this._cloneBoard(board);
+  this.solvePuzzle(clone);
+  
+  var digs = [0,1,2,3,4,5,6,7,8];
+  
+  var r = this._chooseDigit(digs);
+  var c = this._chooseDigit(digs);
+  
+  while (board[r][c] != 0) {
+    r = this._chooseDigit(digs);
+    c = this._chooseDigit(digs);
+  }
+  
+  board[r][c] = clone[r][c];
+  
+};
+
+sudoku.solver.generatePuzzle = function (hintCount) {
+  hintCount = hintCount || 35;
+  var grid = [];
+  grid.push([0,0,0,0,0,0,0,0,0]);
+  grid.push([0,0,0,0,0,0,0,0,0]);
+  grid.push([0,0,0,0,0,0,0,0,0]);
+  grid.push([0,0,0,0,0,0,0,0,0]);
+  grid.push([0,0,0,0,0,0,0,0,0]);
+  grid.push([0,0,0,0,0,0,0,0,0]);
+  grid.push([0,0,0,0,0,0,0,0,0]);
+  grid.push([0,0,0,0,0,0,0,0,0]);
+  grid.push([0,0,0,0,0,0,0,0,0]);
+
+  var digs = [1,2,3,4,5,6,7,8,9];
+  var cols = [0,1,2,3,4,5,6,7,8];
+  var rows = [8,7,6,5,4,3,2,1,0];
+
+  for (var i = 0; i < 9; i++) {
+    var col = this._popDigit(cols);
+    var dig = this._popDigit(digs);
+    var row = this._popDigit(rows);
+    grid[row][col] = dig;
+  }
+
+  var emptys = this.saveEmptyPositions(grid);
+  var solved = this.solvePuzzle(grid, emptys);
+
+  var grid2 = [];
+  grid2.push([0,0,0,0,0,0,0,0,0]);
+  grid2.push([0,0,0,0,0,0,0,0,0]);
+  grid2.push([0,0,0,0,0,0,0,0,0]);
+  grid2.push([0,0,0,0,0,0,0,0,0]);
+  grid2.push([0,0,0,0,0,0,0,0,0]);
+  grid2.push([0,0,0,0,0,0,0,0,0]);
+  grid2.push([0,0,0,0,0,0,0,0,0]);
+  grid2.push([0,0,0,0,0,0,0,0,0]);
+  grid2.push([0,0,0,0,0,0,0,0,0]);
+
+  cols = [0,1,2,3,4,5,6,7,8];
+  rows = [8,7,6,5,4,3,2,1,0];
+//console.log('hinting');
+  for (var i = 0; i <= hintCount; i++) {
+    var r = this._chooseDigit(rows);
+    var c = this._chooseDigit(cols);
+    grid2[r][c] = solved[r][c];
+  }
+
+  return grid2;
+
+};
+sudoku.solver._popDigit = function (availableArray) {
+  var r = Math.floor((Math.random() * availableArray.length - 1) + 1);
+  var d = availableArray[r];
+  availableArray.splice(r, 1);
+  //console.log('_popDigit', 'avail.count', availableArray.length, 'took', d);
+  return d;
+};
+sudoku.solver._chooseDigit = function (availableArray) {
+  var r = Math.floor((Math.random() * availableArray.length - 1) + 1);
+  var d = availableArray[r];
+  //availableArray.splice(r, 1);
+  //console.log('_chooseDigit', 'avail.count', availableArray.length, 'took', d);
+  return d;
+};
+
+sudoku.solver.isSolvable = function (board) {
+  try {
+    var clone = this._cloneBoard(board);
+    this.solvePuzzle(clone);
+    return true;
+  } catch (ex) {
+    return false;
+  }
+};
+sudoku.solver._cloneBoard = function (board) {
+  var clone = [];
+  for (var i = 0; i < board.length; i++) {
+    clone.push([]);
+    for (var j = 0; j < board[i].length; j++) {
+      clone[i].push(board[i][j]);
+    }
+  }
+  return clone;
+};
